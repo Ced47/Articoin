7a498b6cce8a641e23221fc0db19e2c7dd3080ee
mcodjo6@gmail.com <cedric.codjo@epitech.eu>
2025-05-10 16:21:23 +0100

    update

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -34,7 +34,7 @@ const ProductSchema = new mongoose.Schema({
     producter: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Producter',
-        required: true,
+        required: false,
     },
     createdAt: {
         type: Date,
