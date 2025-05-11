3fd6bafbab2fb96730e9bc0fb4309ef3db7f2f3a
mcodjo6@gmail.com <cedric.codjo@epitech.eu>
2025-05-11 14:19:21 +0100

    update

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -3,11 +3,10 @@ const categoryController = require('../controllers/categoryController');
 const { authenticate, authorize } = require('../middlewares/auth');
 
 const router = express.Router();
-router.use(authenticate);
 
 router.get('/categories', categoryController.getCategories);
-router.post('/category', categoryController.createCategory);
-router.put('/category/:id', authorize('superstaff'), categoryController.updateCategory);
-router.delete('/category/:id', authorize('superstaff'), categoryController.deleteCategory);
+router.post('/category', authenticate, categoryController.createCategory);
+router.put('/category/:id', authenticate, authorize('superstaff'), categoryController.updateCategory);
+router.delete('/category/:id', authenticate, authorize('superstaff'), categoryController.deleteCategory);
 
 module.exports = router;
