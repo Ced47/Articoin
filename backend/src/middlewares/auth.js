c8a77019cdea7b59e8e1b222fd8be373b3d6547c
mcodjo6@gmail.com <cedric.codjo@epitech.eu>
2025-05-11 16:11:25 +0100

    update

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 exports.authenticate = (req, res, next) => {
     const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({ message: 'Non authentifié' });
+    if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
     }
 
     try {
