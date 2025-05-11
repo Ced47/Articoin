bdf266122e4fe31f868a7b440047a3e21b939ddb
mcodjo6@gmail.com <cedric.codjo@epitech.eu>
2025-05-11 16:41:34 +0100

    update

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -5,6 +5,7 @@ exports.authenticate = (req, res, next) => {
 
     if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
+        next();
     }
 
     try {
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -9,7 +9,7 @@ router.get('/products', productController.getProducts);
 router.get('/products/category/:id', productController.getProductsByCategory);
 router.get('/products/categories', productController.getProductsByCategories);
 router.get('/product/:id', productController.getProduct);
-router.post('/product', authenticate, uploadImages, productController.createProduct);
+router.post('/product', uploadImages, productController.createProduct);
 router.put('/product/:id', authenticate, uploadImages, authorize('superstaff'), productController.updateProduct);
 router.put('/product/stock/:id', authenticate, authorize('superstaff'), productController.adjustStock);
 router.delete('/product/:id', authenticate, uploadImages, authorize('superstaff'), productController.deleteProduct);
